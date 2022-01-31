00803fa6d1665f3f598c8222ff235b26f4c9f972
Suraj Keshri <skk2142@columbia.edu>
2022-01-31 16:15:48 -0500

    adds helm

--- .github/helm/entrypoint.sh
+++ .github/helm/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -l
 set -e
 
-echo `pwd`
-echo $KUBECONFIGBASE64
 echo $KUBECONFIGBASE64 | base64 -d > /kubeconfig.yaml
 
 export KUBECONFIG=/kubeconfig.yaml
 
-helm version
-kubectl version
+ls -al
+
+helm template test

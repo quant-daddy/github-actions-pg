7ca45f4dbfad13926668efd7377abd156832a378
Suraj Keshri <skk2142@columbia.edu>
2022-01-31 16:10:24 -0500

    fix

--- .github/helm/entrypoint.sh
+++ .github/helm/entrypoint.sh
@@ -3,8 +3,9 @@ set -e
 
 echo `pwd`
 echo $KUBECONFIGBASE64
-echo $KUBECONFIGBASE64 | base64 -d > kubeconfig.yaml
-cat kubeconfig.yaml
+echo $KUBECONFIGBASE64 | base64 -d > /kubeconfig.yaml
+
+export KUBECONFIG=/kubeconfig.yaml
 
 helm version
 kubectl version
